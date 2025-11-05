20089e717657ef43b618f59b5d73c4e9e449ff55
dagrath <40446415+dagrath@users.noreply.github.com>
2025-11-05 12:41:20 -0600

    Add files via upload

---
+++ ha_control.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import pyautogui
+import time
+
+app = Flask(__name__)
+
+@app.route("/press/<keys>")
+def press(keys):
+    # Example: keys = "ctrl+s" or "alt+f4"
+    combo = keys.split("+")
+    pyautogui.hotkey(*combo)
+    return f"Pressed {keys}"
+
+@app.route("/start_gspro")
+def open_gspro():
+    pyautogui.hotkey("win", "r")
+    pyautogui.write("C:\\GSProV1\\Core\\GSP\\gspro.exe\n")
+    time.sleep(15)
+    pyautogui.hotkey("win","r")
+    pyautogui.write("C:\\GSPro\\GSPro_Skytrak_1.98\\GSProDeviceInterface.exe\n")
+    return "Running"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
